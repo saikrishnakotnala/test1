6f51f74b13f03058908f49b616b5999ab390ef6c
Sai krishna kotnala <saikrishnachinnu8@gmail.com>
2018-11-08 01:16:29 +0530

    Update sendit.php

--- sendit.php
+++ sendit.php
@@ -12,7 +12,7 @@ $msg = $str1 . ' ' . $str2;
 
     include('way2sms-api.php');
    
-    sendWay2SMS ( '8328051728' , '08feb1996' , '984946183' , $str1);   
+    sendWay2SMS ( '8328051728' , '08feb1996' , '9849461283' , $str1);   
     echo "sent";
      
 ?>
